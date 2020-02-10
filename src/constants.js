575da2ea3478bf91e5d1dbd7f910d519c119ca06
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-02-10 17:02:12 +0200

    remove constant chunk_size

--- src/constants.js
+++ src/constants.js
@@ -39,8 +39,6 @@ export const QUEUE_ITEM_STATE = {
 
 export const OPERATIONS = {CREATE: 'CREATE', UPDATE: 'UPDATE'};
 
-export const CHUNK_SIZE = 100;
-
 export const conversionFormats = {
 	MARCXML: 1,
 	ISO2709: 2,
