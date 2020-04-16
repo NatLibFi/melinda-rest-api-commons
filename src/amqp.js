8b3b30b8111a343d41c3cceece01351d716c6675
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-04-16 15:10:24 +0300

    debug get data

--- src/amqp.js
+++ src/amqp.js
@@ -223,6 +223,8 @@ export default async function (AMQP_URL) {
 
       const messages = await Promise.all(messagesToGet);
 
+      console.log(messages); // eslint-disable-line no-console
+
       const uniqMessages = messages.filter(onlyUniques);
 
       return uniqMessages;
