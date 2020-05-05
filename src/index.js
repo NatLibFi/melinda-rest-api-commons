d6f0dd9739167ca31d7156b3e59908da2b85fe0c
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-05-05 08:56:28 +0300

    helppers to helpers

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@
 
 import mongoFactory from './mongo';
 import amqpFactory from './amqp';
-import conversions from './helppers/conversion';
-import validations from './helppers/validation';
+import conversions from './helpers/conversion';
+import validations from './helpers/validation';
 import {logError} from './utils';
 
 export {mongoFactory, amqpFactory, conversions, validations, logError};
