5bd08ab4867a941d62e702d6b88706816a7293f8
Minttu Hurme <ammsalme@users.noreply.github.com>
2022-04-05 15:12:58 +0300

    Update recordResponseItem

--- src/utils.js
+++ src/utils.js
@@ -67,43 +67,64 @@ export function createImportJobState(operation, state, queryImportJobState = fal
 }
 
 export function createRecordResponseItem({responsePayload, responseStatus, recordMetadata, id}) {
-  const recordResponseStatus = getRecordResponseStatus(responseStatus, responsePayload);
+  const recordResponseStatusAndMessage = getRecordResponseStatusAndMessage(responseStatus, responsePayload);
   const recordResponseItem = {
     melindaId: id || '000000000',
-    recordMetadata: recordMetadata || undefined,
-    ...recordResponseStatus
+    recordMetadata: responsePayload.recordMetadata || recordMetadata || undefined,
+    ...recordResponseStatusAndMessage
   };
   return recordResponseItem;
 }
 
-export function getRecordResponseStatus(responseStatus, responsePayload) {
+function getRecordResponseStatusAndMessage(responseStatus, responsePayload) {
 
-  logger.verbose(`Response status: ${responseStatus} responsePayload: ${JSON.stringify(responsePayload)}`);
+  logger.debug(`Response status: ${responseStatus} responsePayload: ${JSON.stringify(responsePayload)}`);
   const responseStatusName = httpStatus[`${responseStatus}_NAME`];
-  logger.verbose(`Response status name: ${responseStatusName}`);
+  logger.debug(`Response status name: ${responseStatusName}`);
+
+  const message = getMessageFromResponsePayload(responsePayload);
+
+  logger.debug(`Response message: ${message}`);
 
   // Non-http statuses
-  if (['UPDATED', 'CREATED', 'INVALID', 'ERROR', 'UNKNOWN'].includes(responseStatus)) {
-    return {status: responseStatus, message: responsePayload};
+  if (['UPDATED', 'CREATED', 'INVALID', 'ERROR'].includes(responseStatus)) {
+    return {status: responseStatus, message};
   }
 
-  // Duplicates and other conflicts
+  if (['UNKNOWN'].includes(responseStatus)) {
+    const possibleIds = responsePayload.ids || [];
+    return {status: responseStatus, message, possibleIds};
+  }
+
+  // Duplicates and other CONFLICT statuses
   if ([httpStatus.CONFLICT, 'CONFLICT'].includes(responseStatus)) {
-    if ((/^Duplicates in database/u).test(responsePayload.message)) {
-      return {status: 'DUPLICATE', message: responsePayload.message, duplicateIds: responsePayload.ids};
+    if ((/^Duplicates in database/u).test(message)) {
+      const duplicateIds = responsePayload.ids || [];
+      return {status: 'DUPLICATE', message, duplicateIds};
     }
-    return {status: 'CONFLICT', message: responsePayload.message};
+    return {status: 'CONFLICT', message};
   }
 
   if ([httpStatus.UNPROCESSABLE_ENTITY, 'UNPROCESSABLE_ENTITY'].includes(responseStatus)) {
-    return {status: 'UNPROCESSABLE_ENTITY', message: responsePayload};
+    return {status: 'UNPROCESSABLE_ENTITY', message};
   }
 
   if ([httpStatus.NOT_FOUND, 'NOT_FOUND'].includes(responseStatus)) {
-    return {status: 'NOT_FOUND', message: responsePayload};
+    return {status: 'NOT_FOUND', message};
   }
 
-  return {status: 'ERROR', message: responsePayload};
+  // Otherwise
+  return {status: 'ERROR', message};
+}
+
+function getMessageFromResponsePayload(responsePayload) {
+  if (responsePayload.message) {
+    return responsePayload.message;
+  }
+  if (typeof responsePayload === 'string' || responsePayload instanceof String) {
+    return responsePayload;
+  }
+  return '';
 }
 
 export async function addRecordResponseItem({recordResponseItem, correlationId, mongoOperator}) {
