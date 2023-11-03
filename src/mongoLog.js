7827640a9986f79a021be272defa795295c1ebed
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2023-11-03 18:30:06 +0200

    MRA-728 (#142)
    * add different test date format for mongo
    * add JSDoc comments
    * remove luxon dependency
    
    Update serializers (#143)
    * Update serializers + deps
    
    v4.0.8-alpha.12
    
    Co-authored-by: Minttu Hurme <ammsalme@users.noreply.github.com>

--- src/mongoLog.js
+++ src/mongoLog.js
@@ -4,7 +4,7 @@
 *
 * Shared modules for microservices of Melinda rest api batch import system
 *
-* Copyright (C) 2020-2022 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2020-2023 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of melinda-rest-api-commons
 *
@@ -35,6 +35,12 @@ import httpStatus from 'http-status';
 import sanitize from 'mongo-sanitize';
 import {LOG_ITEM_TYPE} from './constants';
 
+/**
+ * Create log operator
+ * @param {String} MONGO_URI connnection address to mongo
+ * @param {String} dbName Mongo DB name, defaults 'rest-api'
+ * @returns {Object} containing all log handling functions
+ */
 export default async function (MONGO_URI, dbName = 'rest-api') {
   const logger = createLogger();
 
@@ -44,6 +50,11 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
   const collection = 'logs';
   return {addLogItem, query, queryById, getListOfLogs, getListOfCatalogers, getExpandedListOfLogs, protect, remove, removeBySequences};
 
+  /**
+   * Add log item to collection
+   * @param {Object} logItem contains log item data
+   * @returns void
+   */
   async function addLogItem(logItem) {
     const time = moment().toDate();
     const newLogItem = {
@@ -70,11 +81,17 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     }
   }
 
-  async function query(params) {
-    logger.debug(`Query params: ${JSON.stringify(params)}`);
-    checkLogItemType(params.logItemType, false, false);
-    const {limit = 5, skip = 0, ...rest} = params;
-    logger.debug(`Actual query params: ${JSON.stringify(rest)}`);
+  /**
+   * Query log items
+   * @param {Integer} limit defaults 5
+   * @param {Integer} skip defaults 0
+   * @param {Object} rest query params
+   * @returns result array
+   */
+  async function query({limit = 5, skip = 0, ...rest}) {
+    logger.debug(`Query params: ${JSON.stringify(rest)}`);
+    logger.debug(`Limit and skip params: ${limit} | ${skip}`);
+    checkLogItemType(rest.logItemType, false, false);
     const result = await db.collection(collection) // eslint-disable-line functional/immutable-data
       .find(rest)
       .sort({creationTime: 1})
@@ -87,10 +104,18 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
   }
 
   // DEVELOP: queryById returns jsut one (randomish) log! is this useful?
-  async function queryById(correlationIdString, skip = 0, limit = 1) {
-    logger.debug(`QueryById: ${correlationIdString}`);
+  /**
+   * Get single log item by correlationId
+   * @param {String} correlationId
+   * @param {String} logItemType constant LOG_ITEM_TYPE defaults LOG_ITEM_TYPE.MERGE_LOG
+   * @param {Integer} skip defaults 0
+   * @param {Integer} limit defaults 1
+   * @returns query result array
+   */
+  async function queryById(correlationId, logItemType = LOG_ITEM_TYPE.MERGE_LOG, skip = 0, limit = 1) {
+    logger.debug(`QueryById: ${correlationId}`);
     const result = await db.collection(collection) // eslint-disable-line functional/immutable-data
-      .find({correlationId: correlationIdString})
+      .find({correlationId, logItemType})
       .sort({blobSequence: 1})
       .skip(parseInt(skip, 10))
       .limit(parseInt(limit, 10))
@@ -101,14 +126,23 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
 
   // getListOfLogs returns list of correlationIds that have logs of given logItemType
   // if logItemType is not given, getListOfLogs returns list of correlationIds that have MERGE_LOG
-  async function getListOfLogs(logItemType = 'MERGE_LOG') {
-    checkLogItemType(logItemType, false, false);
+  /**
+   * Get list of correlationIds of logs filtered by logItemType
+   * @param {String} logItemType constant LOG_ITEM_TYPE defaults LOG_ITEM_TYPE.MERGE_LOG
+   * @returns Array of query results
+   */
+  async function getListOfLogs(logItemType = LOG_ITEM_TYPE.MERGE_LOG) {
+    // checkLogItemType(logItemType, false, false);
     const result = await db.collection(collection) // eslint-disable-line functional/immutable-data
       .distinct('correlationId', {logItemType});
     logger.debug(`Query result: ${result.length > 0 ? `Found ${result.length} log items!` : 'Not found!'}`);
-    return {status: result.length > 0 ? httpStatus.OK : httpStatus.NOT_FOUND, payload: result.length > 0 ? result : 'No logs found'};
+    return result;
   }
 
+  /**
+   * Get list of catalogers from logs
+   * @returns Array of query results
+   */
   async function getListOfCatalogers() {
     logger.debug(`Getting list of Catalogers`);
 
@@ -118,15 +152,23 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     return result;
   }
 
-  // getExpandedListOfLogs returns groped MERGE_LOGs and MATCH_LOGs
-  async function getExpandedListOfLogs({logItemTypes = [LOG_ITEM_TYPE.MERGE_LOG, LOG_ITEM_TYPE.MATCH_LOG], catalogers = [], dateBefore = new Date(), dateAfter = new Date('2000-01-01')}) {
+  /**
+   * Get filtered list of logs with extra info. Give params in Object
+   * @param {[String]} logItemTypes: String array of logItemTypes. Defaults [LOG_ITEM_TYPE.MERGE_LOG, LOG_ITEM_TYPE.MATCH_LOG]
+   * @param {[String]} catalogers: String array of wanted catalogers. Defaults []
+   * @param {String} dateBefore: String date schema 'YYYY-MM-DD'. Defaults new Date()
+   * @param {String} dateAfter: String date schema 'YYYY-MM-DD'. Defaults '2000-01-01'
+   * @param {Boolean} test: Boolean is this test run. Defaults false
+   * @returns Parsed Object {'correlationId', 'logItemType', 'creationTime', 'cataloger', 'logCount'}
+   */
+  async function getExpandedListOfLogs({logItemTypes = [LOG_ITEM_TYPE.MERGE_LOG, LOG_ITEM_TYPE.MATCH_LOG], catalogers = [], dateBefore = new Date(), dateAfter = '2000-01-01', test = false}) {
     logger.debug(`commons: logItemTypes: ${JSON.stringify(logItemTypes)}, dateAfter: ${dateAfter}, dateBefore: ${dateBefore}}, catalogers: ${JSON.stringify(catalogers)}`);
-    logger.debug(JSON.stringify(generateMatchObject(logItemTypes, catalogers, dateBefore, dateAfter))); // eslint-disable-line
+    logger.debug(JSON.stringify(generateMatchObject({logItemTypes, catalogers, dateBefore, dateAfter}))); // eslint-disable-line
     //checkLogItemType(logItemType, false, false);
     logger.debug(`Getting expanded list of logs`);
     const pipeline = [
       // currently return only MERGE_LOG and MATCH_LOG
-      generateMatchObject(logItemTypes, catalogers, dateBefore, dateAfter),
+      generateMatchObject({logItemTypes, catalogers, dateBefore, dateAfter, test}),
       {
         '$sort':
           {'correlationId': 1, 'logItemType': 1, 'creationTime': 1}
@@ -160,18 +202,25 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     logger.silly(`Query result: ${JSON.stringify(fixedResult)}`);
 
     logger.debug(`Query result: ${fixedResult.length > 0 ? `Found ${fixedResult.length} log items!` : 'Not found!'}`);
-    return {status: fixedResult.length > 0 ? httpStatus.OK : httpStatus.NOT_FOUND, payload: fixedResult.length > 0 ? fixedResult : 'No logs found'};
+    return fixedResult;
 
-    function generateMatchObject(logItemTypes, catalogers, dateBefore, dateAfter) {
-      const dateBeforeIso = new Date(dateBefore).toISOString();
-      const dateAfterIso = new Date(dateAfter).toISOString();
+    /**
+     * Generate match object for aggregation pipeline
+     * @param {[String]} logItemTypes: String array of logItemTypes.
+     * @param {[String]} catalogers: String array of wanted catalogers
+     * @param {String} dateBefore: String date schema 'YYYY-MM-DD'
+     * @param {String} dateAfter: String date schema 'YYYY-MM-DD'
+     * @param {Boolean} test: Boolean is this test run. Defaults false
+     * @returns Match Object
+     */
+    function generateMatchObject({logItemTypes, catalogers, dateBefore, dateAfter, test = false}) {
       const matchOptions = {
         '$match': {
           'logItemType': logItemTypes.length > 0 ? {'$in': logItemTypes} : /.*/ui,
           'cataloger': catalogers.length > 0 ? {'$in': catalogers} : /.*/ui,
           'creationTime': {
-            '$gte': dateAfterIso,
-            '$lte': dateBeforeIso
+            '$gte': test ? new Date(dateAfter).toISOString() : new Date(dateAfter),
+            '$lte': test ? new Date(dateBefore).toISOString() : new Date(dateBefore)
           }
         }
       };
@@ -180,6 +229,12 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     }
   }
 
+  /**
+   * Protect logs from cleaning. Forced cleaning defaults 30days
+   * @param {String} correlationId
+   * @param {Integer} blobSequence
+   * @returns result
+   */
   async function protect(correlationId, blobSequence) {
     logger.info(`Protecting in Mongo (${collection}) correlationId: ${correlationId}, blobSequence: ${blobSequence}`);
     const cleanCorrelationId = sanitize(correlationId);
@@ -198,7 +253,7 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
           }
         ]
       );
-      return {status: result.modifiedCount > 0 ? httpStatus.OK : httpStatus.NOT_FOUND, payload: result.modifiedCount > 0 ? result : 'No logs found'};
+      return result;
     } catch (error) {
       const errorMessage = error.payload || error.message || '';
       logError(error);
@@ -206,6 +261,12 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     }
   }
 
+  /**
+   * Remove logs
+   * @param {String} correlationId
+   * @param {Boolean} force defaults false
+   * @returns result
+   */
   async function remove(correlationId, force = false) {
     logger.info(`Removing from Mongo (${collection}) correlationId: ${correlationId}`);
     const clean = sanitize(correlationId);
@@ -213,7 +274,7 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
 
     try {
       const result = await db.collection(collection).deleteMany(filter);
-      return {status: result.deletedCount > 0 ? httpStatus.OK : httpStatus.NOT_FOUND, payload: result.deletedCount > 0 ? result : 'No logs found'};
+      return result;
     } catch (error) {
       const errorMessage = error.payload || error.message || '';
       logError(error);
@@ -221,6 +282,13 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     }
   }
 
+  /**
+   * Remove logs by sequences
+   * @param {String} correlationId
+   * @param {[Integer]} sequences
+   * @param {Boolean} force defaults false
+   * @returns result
+   */
   async function removeBySequences(correlationId, sequences = [], force = false) {
     logger.info(`Removing from Mongo (${collection}) correlationId: ${correlationId}, sequences: ${sequences.length}`);
     const clean = sanitize(correlationId);
@@ -236,7 +304,7 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
 
     try {
       const result = await db.collection(collection).deleteMany(filter);
-      return {status: result.deletedCount > 0 ? httpStatus.OK : httpStatus.NOT_FOUND, payload: result.deletedCount > 0 ? result : 'No logs found'};
+      return result;
     } catch (error) {
       const errorMessage = error.payload || error.message || '';
       logError(error);
@@ -244,8 +312,14 @@ export default async function (MONGO_URI, dbName = 'rest-api') {
     }
   }
 
+  /**
+   * Checks existence and validity of logItemType
+   * @param {String} logItemType constant LOG_ITEM_TYPE
+   * @param {Boolean} errorUnknown defaults false
+   * @param {Boolean} errorNotExisting defaults false
+   * @returns void
+   */
   function checkLogItemType(logItemType, errorUnknown = false, errorNotExisting = false) {
-
     if (logItemType) {
       const typeInLogItemTypes = Object.values(LOG_ITEM_TYPE).indexOf(logItemType) > -1;
       if (typeInLogItemTypes) {
