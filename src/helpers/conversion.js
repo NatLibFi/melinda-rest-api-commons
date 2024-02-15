1f22176b1913826187249278c25676b2c8916f86
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-02-15 15:47:52 +0200

    Maintenance update (#193)
    * Update license to MIT [MRA-40]
    * Node-tests: v16.x -> v19.x
    * Update CODEOWNERS
    
    * 4.1.3-alpha.2

--- src/helpers/conversion.js
+++ src/helpers/conversion.js
@@ -1,30 +1,4 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Shared modules for microservices of Melinda rest api batch import system
-*
-* Copyright (C) 2020-2021 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of melinda-rest-api-commons
-*
-* melinda-rest-api-commons program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* melinda-rest-api-commons is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
+
 
 import httpStatus from 'http-status';
 import {MARCXML, ISO2709, Json, AlephSequential} from '@natlibfi/marc-record-serializers';
