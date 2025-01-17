ba1513ad4665f03c4e3addf611d340ef21920666
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-01-17 11:41:19 +0200

    Update mongodb v6

--- src/mongo.js
+++ src/mongo.js
@@ -44,7 +44,7 @@ export default async function (MONGO_URI, collection) {
   const logger = createLogger();
 
   // Connect to mongo (MONGO)
-  const client = await MongoClient.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+  const client = await MongoClient.connect(MONGO_URI);
   const db = client.db('rest-api');
   const gridFSBucket = new GridFSBucket(db, {bucketName: collection});
 
