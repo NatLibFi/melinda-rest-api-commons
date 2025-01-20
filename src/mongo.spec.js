a20f94b7bca4c3c7acc0c26a11bbf8bfcb7d1368
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-01-20 16:40:53 +0200

    Add tests

--- src/mongo.spec.js
+++ src/mongo.spec.js
@@ -42,14 +42,16 @@ async function initMongofixtures() {
   });
 }
 
-// eslint-disable-next-line max-statements
+// eslint-disable-next-line max-statements, complexity
 async function callback({
   getFixture,
   functionName,
   params,
   preFillDb = false,
   expectedToThrow = false,
-  expectedError = ''
+  expectedErrorMessage = '',
+  expectedErrorStatus = '',
+  contentStream = false
 }) {
 
   const mongoUri = await mongoFixtures.getUri();
@@ -74,17 +76,249 @@ async function callback({
       const formattedResult = formatQueueItem(result);
       expect(formattedResult).to.eql(expectedResult);
     } catch (error) {
-      debug(error);
-      if (expectedToThrow) {
-        debug('Expected to throw');
-        if (expectedError !== '') {
-          expect(error).to.eql(expectedError);
-          return;
-        }
-        return;
-      }
-      debug('Not expexted to throw');
-      throw error;
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'createBulk') {
+    try {
+      debug(`CreateBulk`);
+      debug(JSON.stringify(params));
+      //{correlationId, cataloger, oCatalogerIn, operation, contentType, recordLoadParams, stream, operationSettings}
+      const stream = contentStream ? getFixture({components: ['contentStream'], reader: READERS.STREAM}) : params.stream;
+      const params2 = {...params, stream};
+      await mongoOperator.createBulk(params2);
+      const dump = await mongoFixtures.dump();
+      const [result] = dump.foobar;
+      debug(result);
+      const formattedResult = formatQueueItem(result);
+      expect(formattedResult).to.eql(expectedResult);
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'checkAndSetState') {
+    try {
+      debug(`checkAndSetState`);
+      debug(JSON.stringify(params));
+      //{correlationId, state, errorMessage = undefined, errorStatus = undefined})
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'checkAndSetImportJobState') {
+    try {
+      debug(`checkAndSetImportJobState`);
+      debug(JSON.stringify(params));
+      //{correlationId, operation, importJobState, errorMessage = '', errorStatus = ''}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'query') {
+    try {
+      debug(`query`);
+      debug(JSON.stringify(params));
+      //params, showParams = {}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'createProjection') {
+    try {
+      debug(`createProjection`);
+      debug(JSON.stringify(params));
+      //showParams = {}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'queryById') {
+    try {
+      debug(`queryById`);
+      debug(JSON.stringify(params));
+      //{correlationId, checkModTime = false}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'checkTimeOut') {
+    try {
+      debug(`checkTimeOut`);
+      debug(JSON.stringify(params));
+      //{correlationId}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'remove') {
+    try {
+      debug(`checkTimeOut`);
+      debug(JSON.stringify(params));
+      //params
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'readContent') {
+    try {
+      debug(`readContent`);
+      debug(JSON.stringify(params));
+      //correlationId
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'removeContent') {
+    try {
+      debug(`removeContent`);
+      debug(JSON.stringify(params));
+      //correlationId
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'getOne') {
+    try {
+      debug(`getOne`);
+      debug(JSON.stringify(params));
+      //{queueItemState, importJobState = undefined}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'getStream') {
+    try {
+      debug(`getStream`);
+      debug(JSON.stringify(params));
+      //correlationId
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'pushIds') {
+    try {
+      debug(`pushIds`);
+      debug(JSON.stringify(params));
+      //{correlationId, handledIds, rejectedIds}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'pushMessages') {
+    try {
+      debug(`pushMessages`);
+      debug(JSON.stringify(params));
+      //{correlationId, messages, messageField = 'messages'}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'setState') {
+    try {
+      debug(`setState`);
+      debug(JSON.stringify(params));
+      //{correlationId, state, errorMessage = undefined, errorStatus = undefined}
+
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'setImportJobState') {
+    try {
+      debug(`setImportJobState`);
+      debug(JSON.stringify(params));
+      //{correlationId, operation, importJobState}
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'setOperation') {
+    try {
+      debug(`setOperation`);
+      debug(JSON.stringify(params));
+      //{correlationId, operation}
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'setOperations') {
+    try {
+      debug(`setOperations`);
+      debug(JSON.stringify(params));
+      //{correlationId, addOperation, removeOperation = undefined}
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'addBlobSize') {
+    try {
+      debug(`addBlobSize`);
+      debug(JSON.stringify(params));
+      //{correlationId}
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
+    }
+    return;
+  }
+
+  if (functionName === 'setBlobSize') {
+    try {
+      debug(`setBlobSize`);
+      debug(JSON.stringify(params));
+      //{correlationId,  blobSize}
+    } catch (error) {
+      handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus});
     }
     return;
   }
@@ -100,4 +334,21 @@ function formatQueueItem(queueItem) {
   };
   debug(JSON.stringify(filteredQueueItem));
   return filteredQueueItem;
+}
+
+function handleError({error, expectedToThrow, expectedErrorMessage, expectedErrorStatus}) {
+  debug(error);
+  if (expectedToThrow) {
+    debug('OK. Expected to throw');
+    if (expectedErrorMessage !== '' || expectedErrorStatus !== '') {
+      const errorMessage = error.message || error.payload || '';
+      expect(errorMessage).to.eql(expectedErrorMessage);
+      const errorStatus = error.status || '';
+      expect(errorStatus).to.eql(expectedErrorStatus);
+      return;
+    }
+    return;
+  }
+  debug('NOT OK. Not expexted to throw');
+  throw error;
 }
